9325c2d36980e64be7cd6bca01168da7522828c5
klklko9 <My@qq.com>
2024-12-22 22:35:35 +0800

    添加 my_printf 函数和 USART2 初始化功能

---
+++ Standard/myprintf.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void USART2_Init(void)
+{
+    // 初始化USART2
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+
+    // 配置PA2为USART2_Tx
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // 配置PA3为USART2_Rx
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // 配置USART2参数
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART2, &USART_InitStructure);
+
+    // 使能USART2
+    USART_Cmd(USART2, ENABLE);
+}
+
+
+void my_printf(const char *format, ...)
+{
+    char buffer[100];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+
+    // 逐字符发送数据
+    for (char *ptr = buffer; *ptr != '\0'; ptr++)
+    {
+        // 等待发送缓冲区为空
+        while (!(USART2->SR & USART_SR_TXE))
+            ;
+        // 发送字符
+        USART2->DR = *ptr;
+    }
+}
